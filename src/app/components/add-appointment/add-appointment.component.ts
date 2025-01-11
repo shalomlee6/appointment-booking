c915a7010dd26962823b9ab2f8849e1adf245119
shalom <shalomlee6@gmail.com>
2025-01-11 16:10:21 +0200

    first commit

---
+++ src/app/components/add-appointment/add-appointment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-appointment',
+  imports: [],
+  templateUrl: './add-appointment.component.html',
+  styleUrl: './add-appointment.component.scss'
+})
+export class AddAppointmentComponent {
+
+}
